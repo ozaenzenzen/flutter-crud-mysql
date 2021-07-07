9194d856f51116d38e77860d5fff6d76797826ee
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-07-07 21:51:23 +0700

    add post method, dio error

--- lib/services/connect.dart
+++ lib/services/connect.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
-
 import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 
 class Connect {
+  final dio = Dio();
   Future<List> getData() async {
-    Dio dio = Dio();
+    // dio = Dio();
     String url = "http://10.0.2.2/flutter_crud_mysql1/get_data.php";
 
     final response = await dio.get(url);
@@ -16,5 +17,32 @@ class Connect {
     } else {
       throw ("Data tidak ditemukan");
     }
+  }
+
+  void addData(var itemcode, var itemname, var price, var stock) {
+    // dio = Dio();
+    String url = "http://10.0.2.2/flutter_crud_mysql1/add_data.php";
+
+    var data = {
+      'item_code': itemcode,
+      'item_name': itemname,
+      'price': price,
+      'stock': stock,
+    };
+    // http.post(
+    //   Uri.parse(url),
+    //   body: data,
+    // );
+
+    dio.post(
+      url,
+      data: {
+        'item_code': 'x',
+        'item_name': 'x',
+        'price': 'x',
+        'stock': 'x',
+      },
+      // options: Options(method: 'POST'),
+    );
   }
 }
