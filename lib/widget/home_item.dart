b031c190b009c789cbbfcbb55b9dc2ffd8d2cd27
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-07-07 22:42:51 +0700

    update widget

--- lib/widget/home_item.dart
+++ lib/widget/home_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud_mysql_1/screens/addpage.dart';
+import 'package:get/get.dart';
 
 class HomeItem extends StatefulWidget {
   final List? data;
@@ -14,60 +16,71 @@ class HomeItem extends StatefulWidget {
 }
 
 class _HomeItemState extends State<HomeItem> {
+  String type = "edit";
   @override
   Widget build(BuildContext context) {
     int index = widget.index;
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      height: 80,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.black26),
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: [
-            BoxShadow(
-                offset: Offset(0, 3),
-                blurRadius: 0.5,
-                spreadRadius: 0.5,
-                color: Colors.black38)
-          ]),
+    dynamic data = widget.data;
+    return InkWell(
+      onTap: () {
+        Get.to(() => AddPage(
+              data: data,
+              index:index,
+              type: type,
+            ));
+      },
       child: Container(
-        margin: EdgeInsets.symmetric(
-          horizontal: 10,
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.apps,
-              size: 40,
-              color: Colors.grey,
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.data![index]['item_name'],
-                  style: TextStyle(
-                    fontFamily: "SF Text",
-                    fontSize: 18,
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: 80,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.black26),
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(0, 3),
+                  blurRadius: 0.5,
+                  spreadRadius: 0.5,
+                  color: Colors.black38)
+            ]),
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: 10,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.apps,
+                size: 40,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.data![index]['item_name'],
+                    style: TextStyle(
+                      fontFamily: "SF Text",
+                      fontSize: 18,
+                    ),
                   ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  "Stocks: ${widget.data![index]['stock']}",
-                  style: TextStyle(
-                    fontFamily: "SF Text",
-                    fontSize: 14,
+                  SizedBox(height: 5),
+                  Text(
+                    "Stocks: ${widget.data![index]['stock']}",
+                    style: TextStyle(
+                      fontFamily: "SF Text",
+                      fontSize: 14,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
