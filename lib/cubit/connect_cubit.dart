782cedf46c09cb773ec9a6441f7f5eb518118fb4
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-07-09 05:53:09 +0700

    update cubit

--- lib/cubit/connect_cubit.dart
+++ lib/cubit/connect_cubit.dart
@@ -1,8 +1,15 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_crud_mysql_1/services/connect.dart';
 import 'package:meta/meta.dart';
 
 part 'connect_state.dart';
 
 class ConnectCubit extends Cubit<ConnectState> {
   ConnectCubit() : super(ConnectInitial());
+
+  void getData(){
+    Connect connect = Connect();
+    // state = connect.getData();
+    // emit(state = connect.getData());
+  }
 }
