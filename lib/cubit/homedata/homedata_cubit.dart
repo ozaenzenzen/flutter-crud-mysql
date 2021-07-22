92065ee5aedfcb9b987df7a7ca22564d5c05af5e
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-07-23 01:15:47 +0700

    update homedata cubit

--- lib/cubit/homedata/homedata_cubit.dart
+++ lib/cubit/homedata/homedata_cubit.dart
@@ -1,8 +1,27 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_crud_mysql_1/model/itemdata.dart';
+import 'package:flutter_crud_mysql_1/services/homedata_services.dart';
 import 'package:meta/meta.dart';
 
 part 'homedata_state.dart';
 
 class HomedataCubit extends Cubit<HomedataState> {
   HomedataCubit() : super(HomedataInitial());
+
+  Homedata _homedata = Homedata();
+
+  void getItemListData() async {
+    emit(HomedataLoading());
+
+    try {
+      final _data = await _homedata.getDataCubit();
+
+      _data.fold(
+        (l) => emit(HomedataError(l)),
+        (r) => emit(HomedataGetSuccess(r)),
+      );
+    } catch (e) {
+      emit(HomedataError(e.toString()));
+    }
+  }
 }
