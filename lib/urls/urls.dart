56938eee2dfbf139469bb576034945a4a815b965
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-08-05 23:49:09 +0700

    update url

--- lib/urls/urls.dart
+++ lib/urls/urls.dart
@@ -1,8 +1,8 @@
 class Urls{
   // static const String BASE_URL = "http://fauzancrud1.infinityfreeapp.com/phpfile/";
-  // static const String BASE_URL = "http://10.0.2.2/flutter_crud_mysql1/";
+  static const String BASE_URL = "http://10.0.2.2/flutter_crud_mysql1/";
 
   // ngrok tunnel http
-  static const String BASE_URL = "http://4ed372344d85.ngrok.io/flutter_crud_mysql1/";
+  // static const String BASE_URL = "http://4ed372344d85.ngrok.io/flutter_crud_mysql1/";
   
 }
