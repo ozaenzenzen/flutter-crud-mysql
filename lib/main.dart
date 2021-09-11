93437bb7b2cf1eca3cd09c6838e614a241d8e892
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-11 18:49:35 +0700

    update main dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:flutter_crud_mysql_1/model/user_model.dart';
 import 'package:flutter_crud_mysql_1/screens/adminpage.dart';
 import 'package:flutter_crud_mysql_1/screens/main_screens/mainpage.dart';
 import 'package:flutter_crud_mysql_1/screens/loginpage.dart';
-import 'package:flutter_crud_mysql_1/services/homedata_services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
