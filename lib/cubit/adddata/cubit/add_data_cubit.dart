13db6effe5e8d62d5f59d03e5847b74f113c38b8
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-11 17:52:00 +0700

    update add data cubit

--- lib/cubit/adddata/cubit/add_data_cubit.dart
+++ lib/cubit/adddata/cubit/add_data_cubit.dart
@@ -1,8 +1,77 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_crud_mysql_1/model/itemdata.dart';
+import 'package:flutter_crud_mysql_1/services/adddata_services.dart';
 import 'package:meta/meta.dart';
 
 part 'add_data_state.dart';
 
 class AddDataCubit extends Cubit<AddDataState> {
   AddDataCubit() : super(AddDataInitial());
+  AddDataServices _addDataServices = AddDataServices();
+
+  void getItemListData() async {
+    emit(AddDataLoading());
+
+    try {
+      final _data = await _addDataServices.getDataCubit();
+
+      _data.fold(
+        (l) => emit(AddDataError(l)),
+        (r) => emit(AddDataGetSuccess(r)),
+      );
+    } catch (e) {
+      emit(AddDataError(e.toString()));
+    }
+  }
+
+  void addItemListData(ItemData itemData) async {
+    emit(AddDataLoading());
+
+    try {
+      _addDataServices.addData(itemData).onError(
+            (error, stackTrace) => emit(
+              AddDataError(error.toString()),
+            ),
+          );
+      emit(AddDataSuccess());
+
+      // final _data = await _addDataServices.addDataCubit(itemData);
+      
+      // _data.fold(
+      //   (l) => emit(AddDataError(l)),
+      //   (r) => emit(AddDataGetSuccess(r)),
+      // );
+    } catch (e) {
+      emit(AddDataError(e.toString()));
+    }
+  }
+
+  void editItemListData(ItemData itemData) async {
+    emit(AddDataLoading());
+
+    try {
+      final _data = await _addDataServices.editDataCubit(itemData);
+
+      _data.fold(
+        (l) => emit(AddDataError(l)),
+        (r) => emit(AddDataGetSuccess(r)),
+      );
+
+      // emit(AddDataSuccess());
+    } catch (e) {
+      emit(AddDataError(e.toString()));
+    }
+  }
+
+  void deleteItemListData(ItemData itemData) async {
+    emit(AddDataLoading());
+
+    try {
+      _addDataServices.deleteDataCubit(itemData);
+
+      emit(AddDataSuccess());
+    } catch (e) {
+      emit(AddDataError(e.toString()));
+    }
+  }
 }
