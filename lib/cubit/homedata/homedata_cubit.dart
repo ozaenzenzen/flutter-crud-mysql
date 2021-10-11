59cf5f758da62ee4a2dd8fff0695ff2b801c07a1
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-11 17:34:57 +0700

    update code

--- lib/cubit/homedata/homedata_cubit.dart
+++ lib/cubit/homedata/homedata_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_crud_mysql_1/model/itemdata.dart';
-import 'package:flutter_crud_mysql_1/services/homedata_services.dart';
+import 'package:flutter_crud_mysql_1/services/adddata_services.dart';
 import 'package:meta/meta.dart';
 
 part 'homedata_state.dart';
@@ -9,13 +9,13 @@ part 'homedata_state.dart';
 class HomedataCubit extends Cubit<HomedataState> {
   HomedataCubit() : super(HomedataInitial());
 
-  Homedata _homedata = Homedata();
+  AddDataServices _addDataServices = AddDataServices();
 
   void getItemListData() async {
     emit(HomedataLoading());
 
     try {
-      final _data = await _homedata.getDataCubit();
+      final _data = await _addDataServices.getDataCubit();
 
       _data.fold(
         (l) => emit(HomedataError(l)),
@@ -30,7 +30,7 @@ class HomedataCubit extends Cubit<HomedataState> {
     emit(HomedataLoading());
 
     try {
-      final _data = await _homedata.addDataCubit(itemData);
+      final _data = await _addDataServices.addDataCubit(itemData);
 
       _data.fold(
         (l) => emit(HomedataError(l)),
@@ -46,7 +46,7 @@ class HomedataCubit extends Cubit<HomedataState> {
     emit(HomedataLoading());
 
     try {
-      final _data = await _homedata.editDataCubit(itemData);
+      final _data = await _addDataServices.editDataCubit(itemData);
       
       _data.fold(
         (l) => emit(HomedataError(l)),
@@ -63,7 +63,7 @@ class HomedataCubit extends Cubit<HomedataState> {
     emit(HomedataLoading());
 
     try {
-      _homedata.deleteDataCubit(itemData);
+      _addDataServices.deleteDataCubit(itemData);
 
       emit(HomedataSuccess());
     } catch (e) {
